57b0518cde0d69b258ac9eee9aa8ec0b219751a2
Kevin Davies <kevin@verum.nz>
2021-01-24 14:56:47 +1300

    ...

---
+++ clock/jsx/clock.jsx
@@ -0,0 +1,22 @@
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {currentTime: (new Date()).toLocaleString()}
+        this.launchClock()
+    }
+
+    launchClock() {
+        setInterval(()=>{
+            console.log('Updating time...')
+            this.setState({
+                currentTime: (new Date()).toLocaleString()
+            })
+        }, 1000)
+    }
+
+
+    render() {
+        return <div>{this.state.currentTime}</div>
+    }
+}
